ac882c400984b3db6fc43ea3e96770f91b34bcb8
Alexander Leonov <alex.thunder@tut.by>
2023-07-28 13:57:23 +0300

    feat: add argparser and s3 storage

--- folders_to_db.py
+++ folders_to_db.py
@@ -1,17 +1,20 @@
-ROOT = "/home/ADMSK/asleon17/Projects/mts_connectwizard/gui/flow_backend/ivr_content"
-
-
+import argparse
 import asyncio
 import os
 from uuid import UUID, uuid4
+
 from app.db.repositories.storage import ItemType, StorageRepository
 from app.db.core import session_factory
+from app.s3.connector import S3Connector
+
+from app.settings import get_settings
 
 
 async def _create_folder_items(
     root: str,
     parent_id: UUID | None,
     repo: StorageRepository,
+    s3connector: S3Connector,
 ):
     items = os.listdir(root)
     folders = []
@@ -25,8 +28,11 @@ async def _create_folder_items(
             folders.append(item)
 
     for file in files:
-        repo.create_item(uuid4(), file, ItemType.FILE, parent_id=parent_id)
+        file_id = uuid4()
+        repo.create_item(file_id, file, ItemType.FILE, parent_id=parent_id)
         await repo.commit()
+        with open(os.path.join(root, file), "rb") as file:
+            await s3connector.upload_file(str(file_id), file.read())
 
     for folder in folders:
         full_path = os.path.join(root, folder)
@@ -41,10 +47,25 @@ async def _create_folder_items(
             print(full_path)
 
 
-async def create_database_entities():
+async def create_database_entities(root: str):
+    settings = get_settings()
     async with session_factory() as session:
         repo = StorageRepository(session)
-        await _create_folder_items(ROOT, None, repo)
+        connector = S3Connector(
+            settings.S3_BUCKET_NAME,
+            settings.S3_ACCESS_KEY,
+            settings.S3_SECRET_KEY,
+            settings.S3_ENDPOINT,
+        )
+        async with connector:
+            await _create_folder_items(root, None, repo, connector)
 
 
-asyncio.run(create_database_entities())
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="FoldersToPostgresS3",
+        description="Create files struct in Posgresql database and upload files into S3 from specific location",
+    )
+    parser.add_argument("source")
+    args = parser.parse_args()
+    asyncio.run(create_database_entities(args.source))
